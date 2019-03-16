6e2f0ab98b23586dd6ea0572109ee2560bc860c1
Doug Kerwin <dwkerwi@gmail.com>
2019-03-16 00:16:47 -0400

    store password intent with arid creation complete

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const logger = require('./logger')
+const logger = require('./logger');
+const authorizedRequest = require('./lib/authorizedRequest');
+const PhoneTokenService = require('phone-token-service')
+const config = require("./config");
 
 function lexResponse(sessionAttributes, fulfillmentState, message) {
     return {
@@ -18,13 +21,25 @@ async function dispatchIntent(intentRequest) {
     if (intentName == 'StorePassword') {
         const sessionAttributes = intentRequest.sessionAttributes;
         const slots = intentRequest.currentIntent.slots;
-        const application = slots.Application;
-        logger.debug(`slots: ${JSON.stringify(slots)}`)
+        const rawApplication = slots.Application;
+        logger.debug(`slots: ${JSON.stringify(slots)}`);
+
+        const phoneTokenService = new PhoneTokenService({
+            // salt for hashing algorithm 
+            tokenHashHmac: config.USERTOKEN_HASH_HMAC,
+            // s3 bucket for storing the mapping relationship
+            s3bucket: 'forgotpw-usertokens-dev',
+            // retrieve country code from ip address via https://ipapi.co/country/
+            defaultCountryCode: 'US'
+        });
+        const phone = intentRequest.userId;
+        const userToken = await phoneTokenService.getTokenFromPhone(phone);
+        let arid = await authorizedRequest.generate(userToken, rawApplication);
 
         return lexResponse(
             sessionAttributes,
             'Fulfilled',
-            `Okay, I know you requested to store ${application}`
+            `To store a password for ${rawApplication}, click: \nhttps://app.forgotpw.com/#/store?arid=${arid}`
         );
     }
 
